66444867baa5ea0f3e3c83d9fec8b75faec16a22
Ghost <mbflug@gmail.com>
2022-08-30 17:21:44 -0600

    pushing updated files

--- index.js
+++ index.js
@@ -1,13 +1,35 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require("inquirer");
+const generateMarkdown = require("./utils/generateMarkdown");
+const fs = require("fs");
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+  {
+    type: "input",
+    name: "title",
+    message: "What is the title of the project?",
+  },
+  {
+    type: "input",
+    name: "description",
+    message: "What is the description?",
+  },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+  inquirer.prompt(questions).then((answers) => {
+    console.log(answers);
+    const generatedReadme = generateMarkdown(answers);
+    console.log(generatedReadme);
+    fs.writeFile("./readme.md", generatedReadme, () => {
+      console.log("finished");
+    });
+  });
+}
 
 // Function call to initialize app
 init();
